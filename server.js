097292a592b0bb5d431a86d8d7fe40683c34de67
tinh.doan <tinh.doan@codecomplete.jp>
2022-03-09 17:01:11 +0700

    format file data

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const cheerio = require('cheerio');
 const fs = require('fs');
 const request = require('request-promise');
-const category = "shop";
-for (let y = 1; y < 2; y++) {
+const category = "food";
+
+fs.writeFile('data.json', '[', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
+
+const maxPage = 20;
+for (let y = 1; y <= maxPage; y++) {
   request(
     "https://dribbble.com/search/shots/popular/web-design?q="+category+"&page="+y,
     (error, response, html) => {
       if (!error && response.statusCode == 200) {
-        var list = [];
         const $ = cheerio.load(html); // load HTML
         const ul = $(".shot-thumbnail").find("div > figure"); // find all images
         ul.each(function (i, el) {
@@ -18,18 +24,38 @@ for (let y = 1; y < 2; y++) {
           var content = noscript.split('?')[1].split('alt="')[1];
           var content = content.trim().slice(0,-2);
 
-          list.push({
-            i : i,
+          
+          list = {
+            i : y+'.'+i,
             url: url,
             content: content
-          });
+          };
+          var data = null ;
+          // if(y  == maxPage && i == ul.length-1){
+          //   console.log(y,i);
+          //   data = JSON.stringify(list)
+          //   fs.appendFile('data.json', data, function (err) {
+          //     if (err) throw err;
+          //     console.log('Saved!');
+          //   });
+          //   fs.appendFile('data.json', ']', function (err) {
+          //     if (err) throw err;
+          //     console.log('Saved!');
+          //   });
+          // }else{
+            data = JSON.stringify(list)+','
+            fs.appendFile('data.json', data, function (err) {
+              if (err) throw err;
+              console.log('Saved!');
+            });
+          // }
         });
-        fs.writeFileSync('data'+y+'.json', JSON.stringify(list));
       } else {
         console.log(error);
       }
-    }
-  );
+  });
 }
+
+
 
 
